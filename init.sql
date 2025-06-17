87fed985fac3f0ab629ae77685550021426b5fce
pakulov_de <pakulov_de@devtrix.ru>
2025-06-18 01:20:14 +0500

    скл

--- init.sql
+++ init.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS product (
     name TEXT NOT NULL,
     price INTEGER NOT NULL,
     image TEXT NOT NULL,
+    category text not null,
     characteristic TEXT NOT NULL
 );
 
@@ -23,7 +24,8 @@ CREATE TABLE IF NOT EXISTS orders (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES "user"(id),
     order_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    total_amount INTEGER NOT NULL
+    total_amount INTEGER NOT NULL,
+    payment_method text not null
 );
 
 CREATE TABLE IF NOT EXISTS order_items (
@@ -42,12 +44,12 @@ CREATE TABLE IF NOT EXISTS rental_dates (
 
 -- Insert sample products
 INSERT INTO product (name, price, image, characteristic) VALUES
-    ('Дрель Trigger', 500, 'images/drel.jpg', 'Мощность: 750 Вт, Скорость: 3000 об/мин, Вес: 1.5 кг'),
-    ('Шуруповерт Makita', 400, 'images/shyryp.jpg', 'Мощность: 18 В (аккумуляторный), Крутящий момент: 50 Н·м, Вес: 1.3 кг'),
-    ('Стремянка', 300, 'images/lestnica.png', 'Высота: 4 ступени (примерно 1.5 м), Материал: алюминий, Макс. нагрузка: 150 кг'),
-    ('Набор отверток Центроинструмент', 550, 'images/otwert.webp', 'Количество предметов: 10 шт, Материал рукояток: прорезиненный пластик, Типы наконечников: плоские, крестовые, шестигранные'),
-    ('Болгарка Bosch', 400, 'images/bolgarka.webp', 'Мощность: 1100 Вт, Диаметр диска: 125 мм, Вес: 2.4 кг'),
-    ('Перфоратор Makita', 650, 'images/perforator.webp', 'Мощность: 800 Вт, Энергия удара: 3 Дж, Вес: 2.8 кг')
+    ('Дрель Trigger', 500, 'images/drel.jpg','Дрели и перфораторы', 'Мощность: 750 Вт, Скорость: 3000 об/мин, Вес: 1.5 кг'),
+    ('Шуруповерт Makita', 400, 'images/shyryp.jpg','Шуруповерты', 'Мощность: 18 В (аккумуляторный), Крутящий момент: 50 Н·м, Вес: 1.3 кг'),
+    ('Стремянка', 300, 'images/lestnica.png','Прочее', 'Высота: 4 ступени (примерно 1.5 м), Материал: алюминий, Макс. нагрузка: 150 кг'),
+    ('Набор отверток Центроинструмент', 550, 'images/otwert.webp','Прочее', 'Количество предметов: 10 шт, Материал рукояток: прорезиненный пластик, Типы наконечников: плоские, крестовые, шестигранные'),
+    ('Болгарка Bosch', 400, 'images/bolgarka.webp', 'Болгарки', 'Мощность: 1100 Вт, Диаметр диска: 125 мм, Вес: 2.4 кг'),
+    ('Перфоратор Makita', 650, 'images/perforator.webp', 'Дрели и перфораторы','Мощность: 800 Вт, Энергия удара: 3 Дж, Вес: 2.8 кг')
 ON CONFLICT (id) DO NOTHING; 
 
 SELECT setval('product_id_seq', (SELECT COALESCE(MAX(id), 0) + 1 FROM product), false);
